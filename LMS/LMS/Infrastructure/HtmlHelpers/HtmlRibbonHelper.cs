80a186a29675c7ce501bb85c8744956ff8468a51
Jasmina <jasmina.eminovski@gmail.com>
2018-09-23 01:24:20 +0200

    Styling ribbon and sidebar items

--- LMS/LMS/Infrastructure/HtmlHelpers/HtmlRibbonHelper.cs
+++ LMS/LMS/Infrastructure/HtmlHelpers/HtmlRibbonHelper.cs
@@ -7,88 +7,86 @@ using System.Web.Mvc;
 namespace LMS.Infrastructure.HtmlHelpers
 {
     public static class HtmlRibbonHelper
-   {
-      public static IHtmlString Ribbon(this HtmlHelper htmlHeper, string typeName = "", string activeRibbonItemId = "",
-         List<IHtmlString> ribbonItems = null, string className = "nav nav-pills ribbon")
-      {
-         MvcHtmlString mvcHtmlString = null;
-         string htmlString = string.Empty;
-         ribbonItems.RemoveAll(item => item == null);
-         
-         TagBuilder ribbon = BuildRibbon(className, typeName, ribbonItems, activeRibbonItemId);
-         htmlString = ribbon.ToString();
-         mvcHtmlString = new MvcHtmlString(htmlString);
+    {
+        public static IHtmlString Ribbon(this HtmlHelper htmlHeper, string typeName = "", string activeRibbonItemId = "",
+           List<IHtmlString> ribbonItems = null, string className = "nav nav-pills ribbon")
+        {
+            MvcHtmlString mvcHtmlString = null;
+            string htmlString = string.Empty;
+            ribbonItems.RemoveAll(item => item == null);
 
-         return mvcHtmlString;
-      }
+            TagBuilder ribbon = BuildRibbon(className, typeName, ribbonItems, activeRibbonItemId);
+            htmlString = ribbon.ToString();
+            mvcHtmlString = new MvcHtmlString(htmlString);
 
-      private static TagBuilder BuildRibbon(string className, string typeName, List<IHtmlString> items, string activeId)
-      {
-         TagBuilder ribbon = new TagBuilder("ul");
-         List<TagBuilder> ribbonItems = new List<TagBuilder>();
-         bool hasActiveItem = items.Any(item => item.ToHtmlString().Contains(activeId));
+            return mvcHtmlString;
+        }
 
-         foreach (IHtmlString ribbonItem in items)
-         {
-            TagBuilder ribbonItemTag = BuildRibbonItem(ribbonItem, hasActiveItem, activeId);
-            ribbonItems.Add(ribbonItemTag);
-         }
+        private static TagBuilder BuildRibbon(string className, string typeName, List<IHtmlString> items, string activeId)
+        {
+            TagBuilder ribbon = new TagBuilder("ul");
+            List<TagBuilder> ribbonItems = new List<TagBuilder>();
+            bool hasActiveItem = items.Any(item => item.ToHtmlString().Contains(activeId));
 
-         if (!hasActiveItem)
-         {
-            var activeRibbon = ribbonItems.First();
-            activeRibbon.MergeAttribute("class", "active");
-         }
+            foreach (IHtmlString ribbonItem in items)
+            {
+                TagBuilder ribbonItemTag = BuildRibbonItem(ribbonItem, hasActiveItem, activeId);
+                ribbonItems.Add(ribbonItemTag);
+            }
 
-         foreach (TagBuilder ribbonItem in ribbonItems)
-         {
-            ribbon.InnerHtml += Environment.NewLine + ribbonItem.ToString();
-         }
-         ribbon.MergeAttribute("class", className);
-         ribbon.MergeAttribute("data-ribbon-type", typeName);
-         
-         return ribbon;
-      }
+            if (!hasActiveItem)
+            {
+                var activeRibbon = ribbonItems.First();
+                activeRibbon.MergeAttribute("class", "active");
+            }
 
-      private static TagBuilder BuildRibbonItem(IHtmlString ribbonItem, bool isActive, string activeId)
-      {
-         TagBuilder ribbonItemTag = new TagBuilder("li");
-         string item = ribbonItem.ToHtmlString();
-         if (isActive && item.Contains(activeId))
-         {
-            ribbonItemTag.MergeAttribute("class", "active");
-         }
-         ribbonItemTag.InnerHtml = Environment.NewLine + ribbonItem.ToHtmlString();
+            foreach (TagBuilder ribbonItem in ribbonItems)
+            {
+                ribbon.InnerHtml += Environment.NewLine + ribbonItem.ToString();
+            }
+            ribbon.MergeAttribute("class", className);
+            ribbon.MergeAttribute("data-ribbon-type", typeName);
 
-         return ribbonItemTag;
-      }
+            return ribbon;
+        }
 
-      public static IHtmlString RibbonItem(this HtmlHelper htmlHelper, string id, string label, string description, IHtmlString iconHtmlString = null)
-      {
-         TagBuilder tagBuilder = new TagBuilder("a");
-         IDictionary<string, string> tagBuilderAttributes = tagBuilder.Attributes;
-         MvcHtmlString mvcHtmlString = null;
-         string htmlString = string.Empty;
-         IHtmlString tooltip = htmlHelper.Tooltip(label, description);
+        private static TagBuilder BuildRibbonItem(IHtmlString ribbonItem, bool isActive, string activeId)
+        {
+            TagBuilder ribbonItemTag = new TagBuilder("li");
+            string item = ribbonItem.ToHtmlString();
+            if (isActive && item.Contains(activeId))
+            {
+                ribbonItemTag.MergeAttribute("class", "active");
+            }
+            ribbonItemTag.InnerHtml = Environment.NewLine + ribbonItem.ToHtmlString();
 
-         tagBuilderAttributes.Add("href", "#");
-         tagBuilderAttributes.Add("class", "ribbon-item");
-         tagBuilderAttributes.Add("data-ribbon-item", id);
-         tagBuilderAttributes.Add("title", tooltip.ToHtmlString());
+            return ribbonItemTag;
+        }
 
-         if (iconHtmlString != null)
-         {
-            tagBuilder.InnerHtml = iconHtmlString.ToHtmlString();
-         }
-         else
-         {
-            tagBuilder.InnerHtml = label;
-         }
+        public static IHtmlString RibbonItem(this HtmlHelper htmlHelper, string id, string label, string description,
+            IHtmlString iconHtmlString = null, string style = null)
+        {
+            TagBuilder tagBuilder = new TagBuilder("a");
+            IDictionary<string, string> tagBuilderAttributes = tagBuilder.Attributes;
+            MvcHtmlString mvcHtmlString = null;
+            string htmlString = string.Empty;
+            IHtmlString tooltip = htmlHelper.Tooltip(label, description);
 
-         htmlString = tagBuilder.ToString();
-         mvcHtmlString = new MvcHtmlString(htmlString);
+            tagBuilderAttributes.Add("href", "#");
+            tagBuilderAttributes.Add("class", "ribbon-item");
+            tagBuilderAttributes.Add("data-ribbon-item", id);
+            tagBuilderAttributes.Add("title", tooltip.ToHtmlString());
+            
+            if (iconHtmlString != null)
+                tagBuilder.InnerHtml = iconHtmlString.ToHtmlString();
 
-         return mvcHtmlString;
-      }
-   }
+            if (!string.IsNullOrEmpty(style))
+                tagBuilderAttributes.Add("id", style);
+
+            htmlString = tagBuilder.ToString();
+            mvcHtmlString = new MvcHtmlString(htmlString);
+
+            return mvcHtmlString;
+        }
+    }
 }
--- LMS/LMS/Infrastructure/HtmlHelpers/HtmlSidebarHelper.cs
+++ LMS/LMS/Infrastructure/HtmlHelpers/HtmlSidebarHelper.cs
@@ -5,34 +5,32 @@ using System.Web.Mvc;
 namespace LMS.Infrastructure.HtmlHelpers
 {
     public static class HtmlSidebarHelper
-   {
-      public static IHtmlString SidebarItem(this HtmlHelper htmlHelper, string name, string label, string description, IHtmlString iconHtmlString = null)
-      {
-         TagBuilder tagBuilder = new TagBuilder("a");
-         IDictionary<string, string> tagBuilderAttributes = tagBuilder.Attributes;
-         MvcHtmlString mvcHtmlString = null;
-         string htmlString = string.Empty;
-         IHtmlString tooltip = htmlHelper.Tooltip(label, description);
+    {
+        public static IHtmlString SidebarItem(this HtmlHelper htmlHelper, string name, string label,
+            string description, IHtmlString iconHtmlString = null, string style = null)
+        {
+            TagBuilder tagBuilder = new TagBuilder("a");
+            IDictionary<string, string> tagBuilderAttributes = tagBuilder.Attributes;
+            MvcHtmlString mvcHtmlString = null;
+            string htmlString = string.Empty;
+            IHtmlString tooltip = htmlHelper.Tooltip(label, description);
 
-         tagBuilderAttributes.Add("href", "#");
-         tagBuilderAttributes.Add("class", "btn sidebar-action");
-         tagBuilderAttributes.Add("role", "button");
-         tagBuilderAttributes.Add("data-sidebar-action", name);
-         tagBuilderAttributes.Add("title", tooltip.ToHtmlString());
+            tagBuilderAttributes.Add("href", "#");
+            tagBuilderAttributes.Add("class", "btn sidebar-action");
+            tagBuilderAttributes.Add("role", "button");
+            tagBuilderAttributes.Add("data-sidebar-action", name);
+            tagBuilderAttributes.Add("title", tooltip.ToHtmlString());
+            
+            if (iconHtmlString != null)
+                tagBuilder.InnerHtml = iconHtmlString.ToHtmlString();
 
-         if (iconHtmlString != null)
-         {
-            tagBuilder.InnerHtml = iconHtmlString.ToHtmlString();
-         }
-         else
-         {
-            tagBuilder.InnerHtml = label;
-         }
+            if (!string.IsNullOrEmpty(style))
+                tagBuilderAttributes.Add("id", style);
 
-         htmlString = tagBuilder.ToString();
-         mvcHtmlString = new MvcHtmlString(htmlString);
+            htmlString = tagBuilder.ToString();
+            mvcHtmlString = new MvcHtmlString(htmlString);
 
-         return mvcHtmlString;
-      }
-   }
+            return mvcHtmlString;
+        }
+    }
 }
